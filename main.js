76f7bfdcbfb0e05bf2534a56c98458655933e432
Fredisma0169 <97203352+Fredisma0169@users.noreply.github.com>
2023-01-04 13:28:56 +0100

    suite ok 1

---
+++ main.js
@@ -0,0 +1,5 @@
+const keys = document.querySelectorAll('.keys');
+const display_input = document.querySelector('.display .input');
+const display_output = document.querySelector('.display .output');
+
+let input = "";
